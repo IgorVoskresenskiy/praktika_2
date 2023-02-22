4e687fb9e207f44dedf1bcfd7e7df12b09d8f217
DESKTOP-93RNQIJ\msi <voskres04@gmail.com>
2023-02-22 17:19:04 +0500

    parser updated

--- multithread.c
+++ multithread.c
@@ -12,13 +12,13 @@
 
 int id = 0;
 int n = 0;
-int command = 0;
 
-char argc[25] = { 0 };
+int argc = 0;
 char argv[4][20] = { 0 };
 
 typedef struct rngNode
 {
+    int rngValue;
     int rngNumber;
     int rngTimeAsked;
     struct rngNode* next;
@@ -28,17 +28,28 @@ rngNode* rngListHead = NULL;
 void command_parse(char* inputCommand)
 {
     int i = 0;
-    int counter = 0;
-    int argv2 = 0;
-    char argcToConvert[5] = { 0 };
     while (inputCommand[i] != '\0')
     {
-        while ((inputCommand[i] != ' ') && (inputCommand[i] != '\0'))
+        if (inputCommand[i] == ' ')
+        {
+            argc++;
+        }
+        i++;
+    }
+    argc++;
+    int counter = 0;
+    int argv2 = 0;
+    int spaceCount = 0;
+    i = 0;
+    while ((inputCommand[i] != '\0') && (spaceCount < argc))
+    {
+        while (inputCommand[i] != ' ')
         {
             argv[counter][argv2] = inputCommand[i];
             i++;
             argv2++;
         }
+        spaceCount++;
         argv2 = 0;
         i++;
         counter++;
@@ -85,23 +96,28 @@ int count_rng_summ(rngNode* rngHead)
     }
     return rngSumm;
 }
-uint8_t buf[1024];
 
-DWORD WINAPI MyThreadFunction(int n)
+DWORD WINAPI MyThreadFunction(int n) //needs to only update the rng list
 {
-    int rngSumm = 0;
     while (true)
     {
-        n += 1;
-        printf_s("%d\n", n);
-        rngSumm = count_rng_summ(rngListHead);
+        //n += 1;
+        //printf_s("%d\n", n);
+        
         Sleep(1000);
-
     }
 }
 
 int main()
 {
+    CreateThread(
+        NULL,                   // default security attributes
+        0,                      // use default stack size  
+        MyThreadFunction,       // thread function name
+        NULL,                   // argument to thread function 
+        0,                      // use default creation flags 
+        &id);
+
     printf("Commands for rng actions: ");
     printf("\n\n");
     printf("To print the amount of rngs working type: \"count_rngs\"");
@@ -116,42 +132,30 @@ int main()
     uint8_t commandInput[20] = { 0 };
     gets_s(commandInput, 20);
     command_parse(commandInput);
-    for (int i = 0; i < 20; i++)
-    {
-        commandInput[i] = 0;
-    }
-
-    CreateThread(
-        NULL,                   // default security attributes
-        0,                      // use default stack size  
-        MyThreadFunction,       // thread function name
-        NULL,                   // argument to thread function 
-        0,                      // use default creation flags 
-        &id);
     
     while (strstr(argv[0], "exit") == NULL)
     {
         if (strstr(argv[0], "count_rngs") != NULL)
         {
-            command = 1;
+            printf_s(count_rngs(rngListHead));
         }
 
         if (strstr(argv[0], "print_summ") != NULL)
         {
-            command = 2;
+            printf_s(count_rng_summ(rngListHead));
         }
 
         if (strstr(argv[0], "add_rng") != NULL)
         {
-            command = 3;
+            if (!push_rng(rngListHead, atoi(argv[1]), atoi(argv[2])))
+            {
+                printf_s("there is no free memory");
+            }
         }
 
         gets_s(commandInput, 20);
         command_parse(commandInput);
-        for (int i = 0; i < 20; i++)
-        {
-            commandInput[i] = 0;
-        }
+
        /* printf_s("%s\n", __FUNCTION__);
         Sleep(100);*/
     }
