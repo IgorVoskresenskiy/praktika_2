f0c938706d77059650638b71bea2e5617dcf3e45
DESKTOP-93RNQIJ\msi <voskres04@gmail.com>
2023-03-09 22:35:20 +0500

    minor fixes

--- multithread.c
+++ multithread.c
@@ -1,6 +1,7 @@
 #include "multithread.h"
 
 #define ONE_SECOND_IN_MS 1000
+#define MAX_ARGC 3
 
 typedef struct rngNode
 {
@@ -13,46 +14,54 @@ typedef struct rngNode
 rngNode* rngListHead = NULL;
 
 HANDLE rngListMutex;
+int rngSumm = 0;
+int rngCount = 0;
 int threadID = 0;
 int argc = 0;
 char argv[4][20] = { 0 };
 
-void command_parse(char* inputCommand)
+bool command_parse(char* inputCommand)
 {
-    memset(argv, 0, 80);
+    //char* ptrToArg = argv;
+
+    memset(argv, 0, sizeof(argv));
     argc = 0;
     int i = 0;
-    while (inputCommand[i] != '\0')
+
+    char* ptrToParsingString = inputCommand;
+    char* ptrToStartOfArg = inputCommand;
+
+    while (ptrToParsingString = strchr(ptrToParsingString, ' '))
     {
-        if (inputCommand[i] == ' ')
+        uint8_t sizeOfParsedArg = ptrToParsingString - ptrToStartOfArg;
+
+        memcpy(argv[argc], ptrToStartOfArg, sizeOfParsedArg);
+
+        if (ptrToParsingString == NULL)
+            break;
+
+        argc++;
+        // ћы точно знаем, что после последнего пробела будет аргумент, поэтому количество аргументов равно количеству пробелов + 1. “.е.
+        // ¬ цикле гаранитрировано не можем получить больше 3 аргуменов.
+        if (argc > 3)
         {
-            argc++;
+            return false;
         }
-        i++;
+
+        ptrToParsingString++;
+        ptrToStartOfArg = ptrToParsingString;
     }
+
+    uint8_t sizeOfParsedArg = inputCommand + strlen(inputCommand) - ptrToStartOfArg;
+    memcpy(argv[argc], ptrToStartOfArg, sizeOfParsedArg);
+
     argc++;
-    int counter = 0;
-    int argv2 = 0;
-    int spaceCount = 0;
-    i = 0;
-    for (int c = 0; c < argc; c++)
-    {
-        while ((inputCommand[i] != ' ')&& (inputCommand[i] != '\0'))
-        {
-            argv[counter][argv2] = inputCommand[i];
-            i++;
-            argv2++;
-        }
-        argv2 = 0;
-        i++;
-        counter++;
-    }
 }
 
-bool push_rng(rngNode** rngHead, int rngNumberInput, int rngTimeAskedInput)
+bool add_rng(rngNode** rngHead, int rngNumberInput, int rngTimeAskedInput)
 {
-    WaitForSingleObject(rngListMutex,INFINITE);
     rngNode* tmp = (rngNode*)malloc(sizeof(rngNode));
+    WaitForSingleObject(rngListMutex, INFINITE);
     if (tmp != NULL)
     {
         tmp->rngMaxNumber = rngNumberInput;
@@ -60,6 +69,7 @@ bool push_rng(rngNode** rngHead, int rngNumberInput, int rngTimeAskedInput)
         tmp->rngTimeSinceUpdateMs = 0;
         tmp->next = (*rngHead);
         (*rngHead) = tmp;
+        rngCount += 1;
         ReleaseMutex(rngListMutex);
         return true;
     }
@@ -70,39 +80,9 @@ bool push_rng(rngNode** rngHead, int rngNumberInput, int rngTimeAskedInput)
     }
 }
 
-int count_rngs(rngNode* rngHead)
-{
-    int rngCount=0;
-    WaitForSingleObject(rngListMutex,INFINITE);
-    rngNode* tmp = rngHead;
-    while (tmp != NULL)
-    {
-        rngCount += 1;
-        tmp = tmp->next;
-    }
-    ReleaseMutex(rngListMutex);
-    return rngCount;
-}
-
-int count_rng_summ(rngNode* rngHead)
-{
-    int rngSumm = 0;
-    WaitForSingleObject(rngListMutex,INFINITE);
-    rngNode* tmp = rngHead;
-    while (tmp != NULL)
-    {
-        if (tmp->rngValue > 0)
-        {
-            rngSumm += tmp->rngValue;
-        }
-        tmp = tmp->next;
-    }
-    ReleaseMutex(rngListMutex);
-    return rngSumm;
-}
-
 void update_rngs(rngNode* rngHead)
 {
+    rngSumm = 0;
     WaitForSingleObject(rngListMutex,INFINITE);
     rngNode* tmp = rngHead;
     while (tmp != NULL)
@@ -114,6 +94,7 @@ void update_rngs(rngNode* rngHead)
             tmp->rngValue = rands % tmp->rngMaxNumber;
             tmp->rngTimeSinceUpdateMs = 0;
         }
+        rngSumm += tmp->rngValue;
         tmp = tmp->next;
     }
     ReleaseMutex(rngListMutex);
@@ -145,13 +126,18 @@ void multithread_list()
     printf("To exit type: \"exit\"");
     printf("\n");
 
-    uint8_t commandInput[20] = {0};
-    gets_s(commandInput, 20);
-    command_parse(commandInput);
-    
+    uint8_t commandInput[20] = { 0 };
+
     while (strstr(argv[0], "exit") == NULL)
     {
-        if ((strstr(argv[0], "count_rngs") == NULL) && (strstr(argv[0], "add_rng") == NULL) && (strstr(argv[0], "print_summ") == NULL))
+        gets_s(commandInput, 20);
+        if (!command_parse(commandInput))
+        {
+            printf("too much arguments");
+            printf("\n");
+        }
+
+        if ((strstr(argv[0], "count_rngs") == NULL) && (strstr(argv[0], "add_rng") == NULL) && (strstr(argv[0], "print_summ") == NULL) && (strstr(argv[0], "exit") == NULL))
         {
             printf("there is no such command");
             printf("\n");
@@ -168,7 +154,7 @@ void multithread_list()
             {
                 printf("amount of rngs active: ");
                 printf("\n");
-                printf("%d", count_rngs(rngListHead));
+                printf("%d", rngCount);
                 printf("\n");
             }
         }
@@ -184,7 +170,7 @@ void multithread_list()
             {
                 printf("summ of rng values: ");
                 printf("\n");
-                printf("%d", count_rng_summ(rngListHead));
+                printf("%d", rngSumm);
                 printf("\n");
             }
         }
@@ -200,7 +186,7 @@ void multithread_list()
             {
                 int MaxNumber = atoi(argv[1]);
                 int TimeBetweenUpdatesMs = atoi(argv[2]) * ONE_SECOND_IN_MS;
-                if (!push_rng(&rngListHead, MaxNumber, TimeBetweenUpdatesMs))
+                if (!add_rng(&rngListHead, MaxNumber, TimeBetweenUpdatesMs))
                 {
                     printf("there is no free memory");
                     printf("\n");
@@ -212,7 +198,7 @@ void multithread_list()
                 }
             }
         }
-        gets_s(commandInput, 20);
-        command_parse(commandInput);
+        
+
     }
 }
